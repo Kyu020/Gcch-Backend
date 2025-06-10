c06058d283834fe526dbad255b1ba0014b590965
Kyu020 <aaronjanestacio20@gmail.com>
2025-06-10 10:57:39 +0800

    hahhaa

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,73 +3,11 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
-use Illuminate\Session\TokenMismatchException;
 
-class VerifyCsrfToken extends Middleware
+class VerifyCsrfToken
 {
-    /**
-     * The URIs that should be excluded from CSRF verification.
-     *
-     * @var array
-     */
-        protected $except = [
-            '*'
-       ];
-
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     *
-     * @throws \Symfony\Component\HttpFoundation\Exception\BadRequestException
-     */
     public function handle($request, Closure $next)
     {
-        // Skip CSRF for API routes
-            return $next($request);
-    }
-
-    /**
-     * Determine if the HTTP request uses a ‘read’ verb.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-    protected function isReading($request)
-    {
-        return in_array($request->method(), ['HEAD', 'GET', 'OPTIONS']);
-    }
-
-    /**
-     * Determine if the request has a URI that should be excluded from CSRF verification.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-    protected function inExceptArray($request)
-    {
-        foreach ($this->except as $except) {
-            if ($request->is($except)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Determine if the session and input CSRF tokens match.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-    protected function tokensMatch($request)
-    {
-        $token = $request->input('_token') ?: $request->header('X-CSRF-TOKEN');
-
-        return $token && hash_equals($request->session()->token(), $token);
+        return $next($request);
     }
 }
