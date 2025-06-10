526ca2f287c3ebcba32ae1c9edb95108bc2ffb63
Kyu020 <aaronjanestacio20@gmail.com>
2025-06-10 11:30:45 +0800

    sankta

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,11 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'http://localhost:5173')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+        '%s%s',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,gcch-frontend.vercel.app',
+        Sanctum::currentApplicationUrlWithPort()
+    ))),
 
 
     /*
